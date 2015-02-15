fa2e74d8212ca1cfa0543552f17fd36b4cdf6ee3
Adonai <hawkins.dom@gmail.com>
2015-02-15 16:40:45 -0600

    ...

---
+++ Game/Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHit : MonoBehaviour {
+
+	public float hitForce = 5000f;
+	public float timeBetweenHits = 1f;
+	
+	private float moveH, moveV;
+	private float timer;
+
+	void Update ()
+	{
+		timer += Time.deltaTime;
+	}
+
+	void OnTriggerStay2D (Collider2D coll)
+	{
+		if (timer >= timeBetweenHits)
+		{
+			GameObject hitObj = coll.gameObject;
+			//bool playerIsTarget = GetComponentInParent<EnemyMovement> ().playerIsTarget;
+			if (hitObj.name == "Player")
+			{
+				PlayerMovement pm = hitObj.GetComponent<PlayerMovement> ();
+				if (pm.hasGem)
+				{
+					GameObject gem = pm.collectedGem;
+					pm.hasGem = false;
+					gem.GetComponentInChildren<GemCollect> ().isCollected = false;
+					//gem.transform.localPosition = hitObj.transform.position;
+					pm.collectedGem = null;
+
+					Vector3 applyForce = (hitObj.transform.position - transform.position) * hitForce;
+					//hitObj.rigidbody2D.velocity = Vector2.zero;
+					hitObj.rigidbody2D.AddForce (applyForce);
+
+					transform.parent.GetComponent<EnemyMovement> ().LookForGem();
+				}
+			}
+			timer = 0f;
+		}
+	}
+}
